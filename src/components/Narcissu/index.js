51c17be291ba38a438197976dc331f0b4c9c8874
Xleine <919143384@qq.com>
2020-03-04 15:29:08 +0800

    update init

---
+++ src/components/Narcissu/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const Narcissu = () => {
+  return (
+    <div className='narcissu'>
+      <p>welcome</p>
+    </div>
+  )
+}
+
+export default Narcissu
