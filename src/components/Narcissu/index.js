c8c1dc05c1ff3745e1ab8f1e71771c3216da0e32
Xleine <919143384@qq.com>
2020-03-04 16:37:34 +0800

    add slate并且初始化

--- src/components/Narcissu/index.js
+++ src/components/Narcissu/index.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
+import { Slate, Editable, withReact } from 'slate-react'
+import { createEditor } from 'slate'
+import { withHistory } from 'slate-history'
+
 import './index.css'
 
 const Narcissu = () => {
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: '随意输入。。。' }],
+    },
+  ])
+
+  const editor = useMemo(() => withReact(withHistory(createEditor())), [])
+
   return (
-    <div className='narcissu'>
-      <p>welcome</p>
-    </div>
+    <main className='narcissu'>
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={value => setValue(value)}
+      >
+        <Editable />
+      </Slate>
+    </main>
   )
 }
 
