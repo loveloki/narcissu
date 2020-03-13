6fe83cc797cb29d2504dba9c5b614d9ee1196c92
Xleine <919143384@qq.com>
2020-03-13 14:12:43 +0800

    update 在编辑的节点前添加类型提示

--- src/components/element/index.js
+++ src/components/element/index.js
@@ -5,29 +5,32 @@ import { useSelected, useFocused } from 'slate-react'
 
 const Element = props => {
   const { attributes, children, element } = props
+  const selected = useSelected()
+  const focused = useFocused()
+
   switch (element.type) {
     case 'block-quote':
-      return <blockquote {...attributes}>{children}</blockquote>
+      return <blockquote data-tip={'quote'} className={(focused && selected) ? 'tip' : ''} {...attributes}>{children}</blockquote>
     case 'bulleted-list':
-      return <ul {...attributes}>{children}</ul>
+      return <ul data-tip={'ul'} className={(focused && selected) ? 'tip' : ''} {...attributes}>{children}</ul>
     case 'heading-one':
-      return <h1 {...attributes}>{children}</h1>
+      return <h1 data-tip={'h1'} className={(focused && selected) ? 'tip' : ''} {...attributes}>{children}</h1>
     case 'heading-two':
-      return <h2 {...attributes}>{children}</h2>
+      return <h2 data-tip={'h2'} className={(focused && selected) ? 'tip' : ''} {...attributes}>{children}</h2>
     case 'heading-three':
-      return <h3 {...attributes}>{children}</h3>
+      return <h3 data-tip={'h3'} className={(focused && selected) ? 'tip' : ''} {...attributes}>{children}</h3>
     case 'heading-four':
-      return <h4 {...attributes}>{children}</h4>
+      return <h4 data-tip={'h4'} className={(focused && selected) ? 'tip' : ''} {...attributes}>{children}</h4>
     case 'heading-five':
-      return <h5 {...attributes}>{children}</h5>
+      return <h5 data-tip={'h5'} className={(focused && selected) ? 'tip' : ''} {...attributes}>{children}</h5>
     case 'heading-six':
-      return <h6 {...attributes}>{children}</h6>
+      return <h6 data-tip={'h6'} className={(focused && selected) ? 'tip' : ''} {...attributes}>{children}</h6>
     case 'list-item':
       return <li {...attributes}>{children}</li>
     case 'thematic-break':
       return <ThematicBreak { ...props } />
     default:
-      return <p {...attributes}>{children}</p>
+      return <p data-tip={'p'} className={(focused && selected) ? 'tip' : ''} {...attributes}>{children}</p>
   }
 }
 
@@ -36,11 +39,15 @@ const ThematicBreak = ({ attributes, children }) => {
   const focused = useFocused()
 
   return (
-    <div {...attributes}>
+    <div {...attributes} className='block'>
       <div contentEditable={false}>
-        <hr style={
-          { boxShadow: (selected && focused) ? '0 0 0 3px #B4D5FF' : 'none'}
-        } />
+        <hr
+          className={(focused && selected) ? 'tip hrTip' : ''}
+          style={
+            { boxShadow: (selected && focused) ? '0 0 0 3px #B4D5FF' : 'none'}
+          }
+          data-tip={'hr'}
+          />
       </div>
       {children}
     </div>
